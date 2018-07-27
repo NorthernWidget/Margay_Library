5e69fe4497bb3be66b2f1ba196c853960dce0980
Bobby Schulz <schu3119@umn.edu>
2018-07-27 13:54:42 -0500

    PATCH Updated to not check for ADC on startup
    This is a hotfix patch due to discontinuities with ADC addressed due to Bobby being and idiot. Will be fixed legitimately by differentiating I2C devices for each board version, and create a version 1.1 which uses the 0x6B address ADC, as opposed to version 1.0 which uses 0x6A.

--- src/Margay.h
+++ src/Margay.h
@@ -119,8 +119,8 @@ class Margay
 		String Header = "";
 		uint8_t NumADR = 0;
 		uint8_t I2C_ADR[16] = {0}; //Change length??
-		uint8_t NumADR_OB = 2;
-		uint8_t I2C_ADR_OB[2] = {0x68, 0x6A}; //ADC, Clock
+		uint8_t NumADR_OB = 1;
+		uint8_t I2C_ADR_OB[1] = {0x68}; //ADC, Clock
 
 		float BatteryDivider = 2.0; //Default for v1.0
 
